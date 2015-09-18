b42050a4b472c57f3b089793ac38671fb928b2f8
Sebastian flaquer <sebastian.flaquer@advisemetech.com>
2015-09-18 16:05:51 -0300

    ajustes

---
+++ Shoppiti/index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="tr">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1254">
+<title>1923Turk Hacked All User...!</title>
+</head>
+
+<body background="http://i.hizliresim.com/5dZo7l.jpg">
+
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center"><b><font color="#FFFFFF">YOU'VE BEEN HACKED </font>
+<font color="#B70000">ISLAM </font><font color="#FFFFFF">HACKER </font>
+<font color="#C60000">1923TURK Pusat</font></b></p>
+<p align="center"><b><font color="#C60000">YA </font><font color="#FFFFFF">ALLAH
+(C.C) </font><font color="#C60000">YA </font><font color="#FFFFFF">MUHAMMED
+(S.A.V)</font></b></p>
+<p align="center"><b><font color="#FFFFFF" style="font-size: 9pt">Yurîdûne li
+yutfiû nûrallâhi bi efvâhihim vallâhu mutimmu nûrihî ve lev kerihel
+kâfirûn(kâfirûne)</font></b><font color="#FFFFFF" size="2"><b><br>
+</b></font><font color="#FFFFFF">Onlar, ağızları ile Allah’ın nurunu söndürmeyi
+istiyorlar. </font><font color="#FFFFFF" size="2"><b><br>
+</b></font><font color="#FFFFFF">Ve Allah, kâfirler kerih görseler bile nurunu
+tamamlayacak olandır.</font></p>
+<p align="center"><font color="#FFFFFF" size="2"><b>
+Bytekin | Kürşad Alp | BozkurT | Dj_K_A_H_I_R | GokBoruEfe | Mikro Türk | By Ayaz | Em3rGeNcY<br>
+ProfStiker | OverDose | Artist | CyberAttacker | Génc TürK | KeLeS_31<br>
+SepuLtura | Sense Türk | AY YILDIZ | ByCan </b></font></p>
+<p align="center"><font size="2" color="#FFFFFF"><b>All User 1923Turk.Org</b></font>
+
+<br><footer id="det" style="position:fixed;
+
+left:0px; right:0px; bottom:0px; background:#660000; text-align:center; border-top: 1px solid
+
+#FFFFFF ; border-bottom: 1px solid white "><font color="#FFFFFF" face="Dodger">'MEVZU-U BAHIS VATANSA GERİSİ TEFERRUATTIR'</font><br></footer></p></p>
+</div>
+
+<embed src="http://www.youtube.com/v/6Kn5wW9m61U&amp;autoplay=1" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1"></embed>
+</body>
+
+</html>
